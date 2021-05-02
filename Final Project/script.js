6a52d56d9c3250c7579cd12cea29f427278bfb12
marley <mgr106@miami.edu>
2021-05-02 14:46:31 -0400

    broke page with custom cursour

--- Final Project/script.js
+++ Final Project/script.js
@@ -1,7 +1,10 @@
-var elms = document.getElementsByTagName("*");
-var n = elms.length;
-for(var i = 0; i < n; i ++) {
-    if(window.getComputedStyle(elms[i]).cursor == "pointer") {
-        elms[i].style.cursor = "https://i.postimg.cc/0jZkRKVD/052dd0c023d9db3d5244875791c71c54-pixilated-arrow-cursor-by-vexels.png";
-    }
+var cont = document.getElementById('container');
+cont.onmousemove = applyCustomPointer;
+
+var customPointer = document.getElementById('customPointer');
+
+function applyCustomPointer (event) {
+    customPointer.style.left = event.clientX - (customPointer.offsetWidth / 2) + 'px';
+    customPointer.style.top = event.clientY - (customPointer.offsetHeight / 2) + 'px';
 }
+
