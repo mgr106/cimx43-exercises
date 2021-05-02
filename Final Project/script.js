98d60a78f36154deaa2f8d03c83c2fd9d42c1af0
marley <mgr106@miami.edu>
2021-05-02 14:57:33 -0400

    broke mouse and hid, added pick up to image mouse and move link for personal link

--- Final Project/script.js
+++ Final Project/script.js
@@ -1,10 +1,43 @@
-var cont = document.getElementById('container');
-cont.onmousemove = applyCustomPointer;
+// var cont = document.getElementById('container');
+// cont.onmousemove = applyCustomPointer;
 
-var customPointer = document.getElementById('customPointer');
+// var customPointer = document.getElementById('customPointer');
 
-function applyCustomPointer (event) {
-    customPointer.style.left = event.clientX - (customPointer.offsetWidth / 2) + 'px';
-    customPointer.style.top = event.clientY - (customPointer.offsetHeight / 2) + 'px';
+// function applyCustomPointer (event) {
+//     customPointer.style.left = event.clientX - (customPointer.offsetWidth / 2) + 'px';
+//     customPointer.style.top = event.clientY - (customPointer.offsetHeight / 2) + 'px';
+// }
+
+var smile = document.getElementById("smile");
+
+var moving = false;
+
+smile.addEventListener("mousedown", initialClick, false);
+
+
+// this is the position of the cursor upon pick up of image
+function move(e){
+
+  var newX = e.clientX - 50;
+  var newY = e.clientY - 50;
+
+  image.style.left = newX + "px";
+  image.style.top = newY + "px";
+
+  
 }
 
+function initialClick(e) {
+
+  if(moving){
+    document.removeEventListener("mousemove", move);
+    moving = !moving;
+    return;
+  }
+  
+  moving = !moving;
+  image = this;
+
+  document.addEventListener("mousemove", move, false);
+
+}
