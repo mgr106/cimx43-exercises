97cff0cfc782df6e41503c560c9f39de943ce614
marley <mgr106@miami.edu>
2021-05-03 17:55:28 -0400

    deleted audio file, added moveable image, added spotidy spin logo

--- Final Project/script.js
+++ Final Project/script.js
@@ -1,14 +1,42 @@
-// var cont = document.getElementById('container');
-// cont.onmousemove = applyCustomPointer;
-
-// var customPointer = document.getElementById('customPointer');
-
-// function applyCustomPointer (event) {
-//     customPointer.style.left = event.clientX - (customPointer.offsetWidth / 2) + 'px';
-//     customPointer.style.top = event.clientY - (customPointer.offsetHeight / 2) + 'px';
-// }
-
 var smile = document.getElementById("smile");
+
+var moving = false;
+
+smile.addEventListener("mousedown", initialClick, false);
+
+
+// this is the position of the cursor upon pick up of image
+function move(e){
+
+  var newX = e.clientX - 240;
+  var newY = e.clientY - 200;
+
+  image.style.left = newX + "px";
+  image.style.top = newY + "px";
+
+  
+}
+
+function initialClick(e) {
+
+  if(moving){
+    document.removeEventListener("mousemove", move);
+    moving = !moving;
+    return;
+  }
+  
+  moving = !moving;
+  image = this;
+
+  document.addEventListener("mousemove", move, false);
+
+
+
+}
+
+//second moveable image
+
+var smile = document.getElementById("move");
 
 var moving = false;
 
