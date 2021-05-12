cac774050dd91a0d69ed67118f014de6cf208d4c
marley <mgr106@miami.edu>
2021-05-12 00:49:24 -0400

    debug java

--- Final Project/script.js
+++ Final Project/script.js
@@ -1,12 +1,12 @@
 // cursor javascript
-let mouseCursor = document.querySelector(".cursor")
-let lis = document.querySelectorAll("p")
+let mouseCursor = document.querySelector(".cursor");
+let lis = document.querySelectorAll("p");
 
-window.addEventListener("mousemove", cursor)
+window.addEventListener("mousemove", cursor);
 
 function cursor(e) {
-  mouseCursor.style.top = e.pageY + "px"
-  mouseCursor.style.left = e.pageX +"px"
+  mouseCursor.style.top = e.pageY + "px";
+  mouseCursor.style.left = e.pageX +"px";
 }
 // end cursor javascript
 
